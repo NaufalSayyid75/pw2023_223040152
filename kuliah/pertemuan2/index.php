506460c9977e86abe67893b67c138ae0ffcb4af8
NaufalSayyid75 <naufal.223040152@mail.unpas.ac.id>
2023-02-22 20:23:37 +0700

    first commit

---
+++ kuliah/pertemuan2/index.php
@@ -0,0 +1,36 @@
+<?php
+    $nama = "Naufal Sayyid Akbar 223040152";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pendahuluan PHP</title>
+</head>
+<body>
+    
+    <h1>
+        <?php  
+            echo "Hello $nama!";
+        ?>
+    </h1>
+
+    <h2> 
+        <?php 
+            echo "Pemograman Web";
+
+        ?>
+    </h2>
+
+    <p>
+        <?php 
+           echo 10 + "10";
+        ?>
+    </p>
+
+
+</body>
+</html>
